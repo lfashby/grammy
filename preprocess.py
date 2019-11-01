51a5fcca76578cb017b6e1b6f0fb4fe617fb16bd
Lucas <lfeashby@gmail.com>
2019-11-01 01:04:17 -0400

    Adde comments to preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,9 @@
 from nltk.corpus import brown
-import json
+
+# A very quick and imperfect way of getting
+# the sorts of sentences I want to work with.
 
 sentences = brown.sents(categories="belles_lettres")
-
 beginning = "<s>"
 end = "</s>\n"
 refined_sentences = []
@@ -14,11 +15,11 @@ for sentence in sentences:
             uppered = word.upper()
             refined_sentence.append(uppered)
     refined_sentence.append(end)
-    # Remove [["<s>"], ["</s>"]] sentences
+    # Skip [["<s>"], ["</s>"]] sentences
     if len(refined_sentence) > 2:
         refined_sentences.append(refined_sentence)
 
 stringify = [" ".join(string) for string in refined_sentences]
 
-with open ("refined_text.txt", "w") as refined_text:
+with open("refined_text.txt", "w") as refined_text:
     refined_text.writelines(stringify)
